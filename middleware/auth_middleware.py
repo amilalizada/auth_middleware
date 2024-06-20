eefb1742ca286c1d1c306f9979f2d19f9c7c3672
amilalizada <elizade_amil@inbox.ru>
2024-06-20 16:36:23 +0400

    browser storage additions

--- middleware/auth_middleware.py
+++ middleware/auth_middleware.py
@@ -12,7 +12,7 @@ class AuthMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
         token = request.headers.get("Authorization") or request.cookies.get("access_token")
         if token is None:
-            return Response(content="Unauthorized", status_code=401)
+            return Response(content={"status_code": 401, "message": "Unauthorized"}, status_code=401)
         custom_headers = dict(request.headers)
         custom_headers["Authorization"] = token
         if not token.startswith("Bearer "):
@@ -21,6 +21,9 @@ class AuthMiddleware(BaseHTTPMiddleware):
         client = Client(url=self.validation_service_url, headers=custom_headers, timeout=self.timeout)
         check_result = await client.check()
         if not check_result:
-            return Response(content="Unauthorized", status_code=401)
+            return Response(content={"status_code": 401, "message": "Unauthorized"}, status_code=401)
+        
+        response = await call_next(request)
+        response.set_cookie(key="access_token", value=client.cooked_value, httponly=True)
                 
-        return await call_next(request)
+        return response
--- middleware/client.py
+++ middleware/client.py
@@ -6,6 +6,7 @@ class Client:
         self.url = url
         self.headers = headers
         self.timeout = timeout
+        self.cooked_value = None
 
     async def check(self):
         async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=self.timeout)) as session:
@@ -15,6 +16,11 @@ class Client:
                 ssl=False,
             ) as response:
                 if response.status != 200:
+                    return False
+                cookies = response.cookies
+                if "access_token" in cookies:
+                    self.cooked_value = cookies['access_token'].value
+                else:
                     return False
         
         return True
