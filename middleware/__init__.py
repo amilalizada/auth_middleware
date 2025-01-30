34292f31bc70ea7d7f44c7be8579dab6ef34230b
sabuhish <sabuhi.shukurov@gmail.com>
2025-01-30 12:46:44 +0400

    udpates on the package

---
+++ middleware/__init__.py
@@ -0,0 +1,3 @@
+from auth_middleware import AuthMiddleware, LocationEnum
+
+__all__ = ["AuthMiddleware", "LocationEnum"]
--- middleware/client.py
+++ middleware/client.py
@@ -1,29 +1,34 @@
+from typing import Union
+
 import aiohttp
-from fastapi import Response
+
 from .enums import LocationEnum
+
 
 class Client:
     def __init__(self, url: str, headers: dict, timeout: int, location: str, key: str):
         self.url = url
-        self.headers = headers
-        self.timeout = timeout
-        self.cooked_value = None
-        self.header_value = None
-        self.location = location
-        self.key = key
+        self.headers: dict = headers
+        self.timeout: int = timeout
+        self.cooked_value: Union[str, None] = None
+        self.header_value: Union[str, None] = None
+        self.location: Union[str, None] = location
+        self.key: Union[str, None] = key
 
-    async def check(self):
-        async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=self.timeout)) as session:
+    async def check(self) -> bool:
+        async with aiohttp.ClientSession(
+            timeout=aiohttp.ClientTimeout(total=self.timeout)
+        ) as session:
             async with session.get(
                 url=self.url,
                 headers=self.headers,
                 ssl=False,
             ) as response:
-                if not response.status.ok:
+                if not response.status == 200:
                     return False
                 if self.location == LocationEnum.COOKIE.value:
                     self.cooked_value = response.cookies.get(self.key)
                 elif self.location == LocationEnum.HEADER.value:
                     self.header_value = response.headers.get("Authorization")
-        
+
         return True
